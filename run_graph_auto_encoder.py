926848f1a9f08be8356cad7215a7f01953ead75f
Janneke13 <45425403+Janneke13@users.noreply.github.com>
2023-04-20 23:43:00 +0200

    finished the file to run the graph autoencoder

--- run_graph_auto_encoder.py
+++ run_graph_auto_encoder.py
@@ -4,78 +4,75 @@ import torch
 from torch_geometric.nn import GAE
 import utils
 from GCN_model import GCN
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.mixture import GaussianMixture
+import string
+from matplotlib.cm import get_cmap
+import matplotlib.patches as mpatches
 
 """
-Similar to the run_classification_model.py file, but the GAE uses a different way of splitting the data. Also, its
-output is different. Therefore, a different class is created. 
+Uses the GAE to encode everything, will end on 2 coordinates. Then, clusters this data and creates a scatter plot of it.
 
-NOTE: for the classification tasks, the training, validation, and test sets are all already created. As the GAE works
-on an edge-level, the edge splitting function of PyTorch Geometric is used beforehand, which also creates the false
-edges.
-
-Partly based on: https://github.com/pyg-team/pytorch_geometric/blob/master/examples/autoencoder.py.
+Creates and saves three scatter plots --> using random_state 1, 2, and 3 --> to check whether the clustering is stable. 
+Saves a plot of the rec loss and the contingency tables of the clustering as well.
 """
 
 
-def run_gae_model(train_data, val_data, test_data, hidden_nodes, output_nodes, optimizer, learning_rate,
-                  weight_decay, nr_epochs, test=True, record_results=False, path_folder=None):
+def run_gae_model(data_name, data, hidden_nodes, optimizer, learning_rate, weight_decay, nr_epochs, label_map):
     """
     Runs the GAE model and records the results - performs the entire training loop.
-    :param train_data: The pre-created training edges.
-    :param val_data: The pre-created validation edges.
-    :param test_data: The pre-created test edges.
+    :param data_name: The name of the dataset (for documentation purposes).
+    :param data: The data object including the edges and the labels.
     :param hidden_nodes: The number of hidden nodes of the GAE.
-    :param output_nodes: The number of output nodes of the GAE.
     :param optimizer: The optimizer used for the GAE.
     :param learning_rate: The learning rate used for the GAE.
     :param weight_decay: The weight decay used for the optimizer of the GAE.
     :param nr_epochs: The number of epochs used for the GAE.
-    :param test: A Boolean stating whether the test data is the validation or the test data.
-    :param record_results: A Boolean stating whether results need to be stored in CSV files or not.
-    :param path_folder: A path to the folder where the results are stored.
+    :param label_map: The mapping of the labels with label:id.
     :return: A dictionary with the results and the model.
     """
 
     # assert whether aspects of the input data are properly defined
     assert optimizer in ["adam", "sgd", "adagrad"], "Optimizer must be one of: 'adam', 'sgd', 'adagrad'."
 
-    # put everything into place to record the results:
-    if record_results:
-        utils.create_results_folders("GAE", path_folder)
+    # ----- CREATE FILES AND STORE CONFIGURATION --------
+    # create new folder (that does not exist yet)
+    current_test = 1
+    while os.path.exists("results/GAE/" + data_name + "_" + str(current_test)):
+        current_test += 1
 
-        # create new file (that does not exist yet)
-        current_test = 1
-        while os.path.exists("results/GAE/" + path_folder + "/" + str(current_test) + ".csv"):
-            current_test += 1
+    utils.create_results_folders("GAE", data_name + "_" + str(current_test))
 
-        # make a file to record the results
-        file_results = open("results/GAE/" + path_folder + "/" + str(current_test) + ".csv", "w")
-        writer_results = csv.writer(file_results)
+    # make a file to record the results
+    file_results = open("results/GAE/" + data_name + "_" + str(current_test) + "/results.csv", "w")
+    writer_results = csv.writer(file_results)
 
-        # create a header
-        header = ["Epoch", "ReconLossTrain", "ReconLossTest", "AUCTest", "APTest"]
-        writer_results.writerow(header)
+    # create a header
+    header = ["Epoch", "ReconLossTrain"]
+    writer_results.writerow(header)
 
-        # save the configuration --> so it can be checked later!
-        file_config = open("results/GAE/" + path_folder + "/" + str(current_test) + "_config.csv", "w")
-        writer_config = csv.writer(file_config)
+    # save the configuration --> so it can be checked later!
+    file_config = open("results/GAE/" + data_name + "_" + str(current_test) + "/config.csv", "w")
+    writer_config = csv.writer(file_config)
 
-        # create a header for the configuration
-        header_config = ["Number_Epochs", "Hidden_Nodes", "Output_Nodes", "Optimizer",
-                         "Learning_Rate", "Weight_Decay", "Test"]
+    # create a header for the configuration
+    header_config = ["Number_Epochs", "Hidden_Nodes", "Optimizer", "Learning_Rate", "Weight_Decay"]
 
-        # write in all the configuration data
-        config = [nr_epochs, hidden_nodes, output_nodes, optimizer,
-                  learning_rate, weight_decay, test]
+    # write in all the configuration data
+    config = [nr_epochs, hidden_nodes, optimizer, learning_rate, weight_decay]
 
-        # write the configuration and close it --> file is just for recording of results!
-        writer_config.writerow(header_config)
-        writer_config.writerow(config)
-        file_config.close()
+    # write the configuration and close it --> file is just for recording of configuration!
+    writer_config.writerow(header_config)
+    writer_config.writerow(config)
+    file_config.close()
 
     # ---- MODEL ------
+    # using two coordinated, so use this as the number of output nodes
+    output_nodes = 2
+
     # create an encoder with the number of input nodes, hidden nodes, and output nodes as defined
-    encoder = GCN(train_data.num_features, hidden_nodes, output_nodes)
+    encoder = GCN(data.num_features, hidden_nodes, output_nodes)
 
     # create the GAE model --> if decoder is not defined, it is the inner product decoder as given by the original paper
     model = GAE(encoder)
@@ -90,9 +87,6 @@ def run_gae_model(train_data, val_data, test_data, hidden_nodes, output_nodes, o
 
     # create loss lists etc., to use later on
     loss_list_tr = list()
-    loss_list_te = list()
-    ap_list = list()
-    auc_list = list()
 
     # run the training loop:
     for epoch in range(nr_epochs):
@@ -101,10 +95,10 @@ def run_gae_model(train_data, val_data, test_data, hidden_nodes, output_nodes, o
         optim.zero_grad()
 
         # encode everything in latent space using the encoder:
-        latent_space = model.encode(train_data.x, train_data.edge_index)
+        latent_space = model.encode(data.x, data.edge_index)
 
         # calculate the reconstruction loss and perform the backwards operation
-        reconstruction_loss = model.recon_loss(latent_space, train_data.pos_edge_label_index)
+        reconstruction_loss = model.recon_loss(latent_space, data.edge_index)
         reconstruction_loss.backward()
 
         # update the parameters
@@ -113,45 +107,92 @@ def run_gae_model(train_data, val_data, test_data, hidden_nodes, output_nodes, o
         # now, evaluate the model and save the information:
         model.eval()
 
-        # when test mode is used
-        if test:
-            latent_space_test = model.encode(test_data.x, test_data.edge_index)
-            reconstruction_loss_test = model.recon_loss(latent_space_test, test_data.pos_edge_label_index,
-                                                        test_data.neg_edge_label_index)
-            AUC_test, average_precision_test = model.test(latent_space_test, test_data.pos_edge_label_index,
-                                                          test_data.neg_edge_label_index)
-        # when validation mode is used
-        else:
-            latent_space_test = model.encode(val_data.x, val_data.edge_index)
-            reconstruction_loss_test = model.recon_loss(latent_space_test, val_data.pos_edge_label_index,
-                                                        val_data.neg_edge_label_index)
-            AUC_test, average_precision_test = model.test(latent_space_test, val_data.pos_edge_label_index,
-                                                          val_data.neg_edge_label_index)
-
-        # if the results are recorded, then add a row to the csv!
-        if record_results:
-            row = [epoch + 1, reconstruction_loss.item(), reconstruction_loss_test.item(), AUC_test.item(),
-                   average_precision_test.item()]
-            writer_results.writerow(row)
-        # otherwise, print them
-        else:
-            print("Epoch: ", epoch + 1, ", Rec_Loss_Train: ", reconstruction_loss.item(), ", Rec_Loss_Test: ",
-                  reconstruction_loss_test.item(), ", AUC_test: ", AUC_test.item(), ", AP_test: ",
-                  average_precision_test.item())
-
         # add it to the lists as well:
         loss_list_tr.append(reconstruction_loss.item())
-        loss_list_te.append(reconstruction_loss_test.item())
-        ap_list.append(average_precision_test.item())
-        auc_list.append(AUC_test.item())
 
-    # close the file if the results were recorded:
-    if record_results:
-        # close the file
-        file_results.close()
+        row = [epoch, reconstruction_loss.item()]
+        writer_results.writerow(row)
+
+    # close the file
+    file_results.close()
 
     # make a dictionary of the results and the model, so they can be used later if needed
-    results_dict = {"model": model, "loss_list_train": loss_list_tr, "loss_list_test": loss_list_te,
-                    "ap_list": ap_list, "auc_list": auc_list}
+    results_dict = {"model": model, "loss_list_train": loss_list_tr}
+
+    # set it to evaluate, and let the model encode the graph again
+    model.eval()
+    latent_space_final = model.encode(data.x, data.edge_index)
+
+    # put the coordinates in a dataframe:
+    coordinates = pd.DataFrame(latent_space_final.detach().numpy())
+
+    # add the labels and filter the dataframe to only get the labelled entities
+    coordinates['labels'] = data.y.int()
+
+    # set the new labeled coordinates
+    coordinates_labelled = coordinates[coordinates['labels'] != 0]
+
+    # find out how many clusters there need to be
+    nr_clusters = len(coordinates_labelled['labels'].unique())
+
+    # reverse the label mapping so it can be used later
+    mapping_labels = {value: key.split('/')[-1] for key, value in label_map.items()}
+
+    # add the label names
+    coordinates_labelled['label_names'] = coordinates_labelled['labels'].map(mapping_labels)
+
+    # cluster the output, do this three times, with different random states (to check whether it is stable)
+    for random_seed in [1, 2, 3]:
+        plt.figure()
+
+        # cluster the output
+        gmm = GaussianMixture(n_components=nr_clusters, random_state=random_seed)
+        gmm.fit(coordinates_labelled[[0, 1]])
+        predictions = gmm.predict(coordinates_labelled[[0, 1]])
+
+        # set the predictions in there!
+        coordinates_labelled['predictions'] = predictions
+
+        # print the contingency matrix
+        con_table = pd.crosstab(coordinates_labelled['label_names'], predictions)
+        con_table.to_csv("results/GAE/" + data_name + "_" + str(current_test) + "/contigency_"
+                         + str(random_seed) + ".csv")
+
+        # set colors
+        colors = get_cmap("Set1").colors
+
+        # set markers
+        markers = list(string.ascii_uppercase)
+
+        # so we have the correct order for the legend later on
+        handles = []
+        handles_colors = []
+
+        # 0 stands for unlabelled, so start from 1
+        for i in range(1, nr_clusters + 1):
+            # in the clustering from sklearn, it starts with 0 and goes up
+            for j in range(nr_clusters):
+                # find the specific slice that contains the ones with a specific marker and color
+                current_pred_class = coordinates_labelled[(coordinates_labelled['predictions'] == j)
+                                                          & (coordinates_labelled['labels'] == i)]
+
+                # color as the true label, form as the predicted label
+                plt.scatter(current_pred_class[0], current_pred_class[1], marker='$' + markers[j] + '$',
+                            color=colors[i - 1])
+
+                if mapping_labels[i] not in handles:
+                    handles.append(mapping_labels[i])
+                    handles_colors.append(mpatches.Patch(color=colors[i], label=mapping_labels[i]))
+
+        plt.legend(handles=handles_colors)
+
+        # save the figure
+        plt.savefig("results/GAE/" + data_name + "_" + str(current_test) + "/clusterplot_" + str(random_seed) + ".jpeg", dpi=300)
+
+    plt.figure()
+    plt.plot(loss_list_tr)
+    plt.xlabel("Epoch")
+    plt.ylabel("Reconstruction Loss")
+    plt.savefig("results/GAE/" + data_name + "_" + str(current_test) + "/rec_loss" + ".jpeg", dpi=300)
 
     return results_dict
